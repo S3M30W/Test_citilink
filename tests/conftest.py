96d9cdc8252e3a1002e2332a75f71d30d582ee34
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-11 23:28:03 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
+import pytest
+from selenium import webdriver
 
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome('D:\PyCharm\chromedriver.exe')
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
