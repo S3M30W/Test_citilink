d68590fe9f25272b2f4a70e10cdc39a05652d4ac
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-14 19:24:04 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,5 @@ class BasePageLocators():
     COME_BUTTON = (By.XPATH,
                    "//button[@type='submit' and @class='SignIn__button js--SignIn__action_sign-in Button jsButton Button_theme_primary Button_size_m Button_full-width']")
     OPENING_CHECK = (By.XPATH, "//div[@class='HeaderUserName__name']")
+    SEARCH_LINE = (By.CSS_SELECTOR,
+                   "div[class='MainHeader__search'] div div form div div [class='SearchQuickResult__input-wrapper'] div label input")
