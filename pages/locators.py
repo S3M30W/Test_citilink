bd550180da485a0447a47c650f1bcbe1a26e9405
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-14 23:58:20 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -14,9 +14,14 @@ class BasePageLocators():
     TEXT_CHECK = (By.XPATH, "//h1[@class='Heading Heading_level_1 ProductHeader__title']")
     FAVORITES_BUTTON_1 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
     FAVORITES_BUTTON_2 = (
-    By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default ProductCardButton__icon_active']")
+        By.XPATH,
+        "//span[@class='ProductCardButton__icon ProductCardButton__icon_default ProductCardButton__icon_active']")
     FAVORITES_BUTTON_3 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
     TEXT_CHECK_AIR = (By.XPATH, "//h1[@class='Heading Heading_level_1 ProductHeader__title']")
     FAVORITES_BUTTON_4 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
     EDITORIAL_FAVORITES = (By.XPATH,
                            "//button[@class='js--FavouritesLeftSidebar__button-remove-all  Button  jsButton Button_theme_secondary-ghost Button_size_m Button_with-icon Button_full-width']")
+    BUY_EVERYTHING_BUTTON = (By.XPATH,
+                             "//button[@class='js--FavouritesLeftSidebar__button-buy-all  Button  jsButton Button_theme_primary-ghost Button_size_m Button_with-icon Button_full-width']")
+    BASKET_BUTTON = (By.XPATH, "//*[@data-name='basket']")
+    BASKET_CHECK = (By.XPATH, "//*[@class='ProductPrice__price OrderFinalPrice__price-current__price']")
