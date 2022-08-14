24b81409eac8af3234953bcd502644430375b546
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-14 18:18:35 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,6 @@ class BasePageLocators():
                     "// div[@class ='IconAndTextWithCount__text_mainHeader IconAndTextWithCount__text' and contains (text(),'Войти')]")
     EMAIL_LINE = (By.XPATH, "//input[@name='login']")
     PASS_LINE = (By.XPATH, "//input[@name='pass']")
+    COME_BUTTON = (By.XPATH,
+                   "//button[@type='submit' and @class='SignIn__button js--SignIn__action_sign-in Button jsButton Button_theme_primary Button_size_m Button_full-width']")
+    OPENING_CHECK = (By.XPATH, "//div[@class='HeaderUserName__name']")
