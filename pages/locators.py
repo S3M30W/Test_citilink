98b72ad9b684fe92539f6def3a2c2eccf6a80583
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-14 23:44:48 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,12 @@ class BasePageLocators():
     OPENING_CHECK = (By.XPATH, "//div[@class='HeaderUserName__name']")
     SEARCH_LINE = (By.CSS_SELECTOR,
                    "div[class='MainHeader__search'] div div form div div [class='SearchQuickResult__input-wrapper'] div label input")
+    TEXT_CHECK = (By.XPATH, "//h1[@class='Heading Heading_level_1 ProductHeader__title']")
+    FAVORITES_BUTTON_1 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
+    FAVORITES_BUTTON_2 = (
+    By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default ProductCardButton__icon_active']")
+    FAVORITES_BUTTON_3 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
+    TEXT_CHECK_AIR = (By.XPATH, "//h1[@class='Heading Heading_level_1 ProductHeader__title']")
+    FAVORITES_BUTTON_4 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
+    EDITORIAL_FAVORITES = (By.XPATH,
+                           "//button[@class='js--FavouritesLeftSidebar__button-remove-all  Button  jsButton Button_theme_secondary-ghost Button_size_m Button_with-icon Button_full-width']")
