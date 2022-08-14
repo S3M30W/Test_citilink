4571c183c0db541824370a822e1ece9a2d32591c
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-15 00:26:41 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -20,8 +20,14 @@ class BasePageLocators():
     TEXT_CHECK_AIR = (By.XPATH, "//h1[@class='Heading Heading_level_1 ProductHeader__title']")
     FAVORITES_BUTTON_4 = (By.XPATH, "//span[@class='ProductCardButton__icon ProductCardButton__icon_default']")
     EDITORIAL_FAVORITES = (By.XPATH,
-                           "//button[@class='js--FavouritesLeftSidebar__button-remove-all  Button  jsButton Button_theme_secondary-ghost Button_size_m Button_with-icon Button_full-width']")
+                           "//*[@id='content']/div[2]/section/div[1]/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[1]/label[2]/span/svg")
     BUY_EVERYTHING_BUTTON = (By.XPATH,
                              "//button[@class='js--FavouritesLeftSidebar__button-buy-all  Button  jsButton Button_theme_primary-ghost Button_size_m Button_with-icon Button_full-width']")
     BASKET_BUTTON = (By.XPATH, "//*[@data-name='basket']")
     BASKET_CHECK = (By.XPATH, "//*[@class='ProductPrice__price OrderFinalPrice__price-current__price']")
+    DELETE_ALL_BUTTON = (By.XPATH, "//*[@data-label='Очистить список']")
+    DELETE_ALL_BASKET = (
+        By.XPATH, "//*[@id=’content’]/div/div/div[1]/div[1]/div/div[1]/div[1]/div[1]/div[3]/div[2]/svg")
+    CHECK_BASKET_DELETE = (By.XPATH, "//*[@class ='Basket__basket-empty-title']")
+    BAY_BASKET = (By.XPATH, "//*[@data-label='Перейти к оформлению']")
+    ADD_FROM_BASKET_TO_FAVORITES = (By.XPATH, "//*[data-label='Добавить все в избранное']")
